96df0f68e2f6906b53cc33333342a20f934c5e97
Anna Gingle <annagingle977@gmail.com>
2019-04-29 14:25:32 -0500

    Floating
    floating

--- java/script.js
+++ java/script.js
@@ -1,18 +1,19 @@
-//Keydown event for imgs 
+
+//Random image slideshow
+function getRandomNumber(max) {
+  return Math.floor(Math.random() * max);
+}
+
 const img = document.querySelector("img");
 const images = [
-  "image src",
-  "image src",
-  "image src",
-  "image src"
+  "images/20.jpg",
+  "08.jpg",
+  "05.jpg",
+  "13.jpg"
 ];
 
-let index = 0;
-document.addEventListener("keydown", (event) => {
-  if (event.key == "37" && index > 1) {
-  index--;
-  } else if (event.keyCode == 39 && index < (images.length - 1)) {
-    index++;
-  }
-  img.src = images[index]
-});
+setInterval(() => {
+  const i = getRandomNumber(images.length);
+  img.src = images[i];
+}, 2000);
+
