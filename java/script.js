2c379f251cfd4472f2de32aa9a0bba965362c755
Anna Gingle <annagingle977@gmail.com>
2019-05-02 15:23:03 -0500

    style changes
    style changes

--- java/script.js
+++ java/script.js
@@ -17,3 +17,13 @@ setInterval(() => {
   img.src = images[i];
 }, 2000);
 
+
+$(window).scroll(function(e){
+  parallax();
+});
+
+//PARALLAX FUNCTION
+function parallax(){
+  var scrolled = $(window).scrollTop();
+  $('.bg').css('top',-(scrolled*0.1)+'px');
+}
