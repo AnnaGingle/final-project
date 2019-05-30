85e0cc4ceb7bea9d95702fa690915f8544010ff3
AnnaGingle <48488699+AnnaGingle@users.noreply.github.com>
2019-05-29 23:21:27 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -29,16 +29,17 @@ function parallax(element, distance, speed) {
 }
 
 window.addEventListener("scroll", function() {
-    parallax("header", window.scrollY, 0.6);
+    parallax("header", window.scrollY, 0.5);
     parallax("nav", window.scrollY, 1);
+    parallax(".intro-text", window.scrollY, 0.6);
+    parallax("#section-one h2", window.scrollY, 0.2);
     //parallax(".small-rose", window.scrollY, 0.4);
     //parallax(".big-rose", window.scrollY, 0.2);
 });
 
-
-
 navSlide();
 
-///const app = ()=>{
-    //navSlide();
+//const app = ()=>{
+//    navSlide();
+    
 //}
