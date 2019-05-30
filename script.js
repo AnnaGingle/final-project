ace7c32f89ba66635e55c28b43549a5bf6ced4b8
AnnaGingle <48488699+AnnaGingle@users.noreply.github.com>
2019-05-29 23:27:26 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ window.addEventListener("scroll", function() {
     parallax("header", window.scrollY, 0.5);
     parallax("nav", window.scrollY, 1);
     parallax(".intro-text", window.scrollY, 0.6);
-    parallax("#section-one h2", window.scrollY, 0.2);
+    
     //parallax(".small-rose", window.scrollY, 0.4);
     //parallax(".big-rose", window.scrollY, 0.2);
 });
