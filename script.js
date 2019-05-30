659159c89804501951832b694e303d0b53579e0b
AnnaGingle <48488699+AnnaGingle@users.noreply.github.com>
2019-05-29 21:29:25 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,41 @@
 const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navlinks = document.querySelector('.nav-links li');
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-links");
+    const navlinks = document.querySelectorAll(".nav-links li");
    
 
-    burger.addEventListener('click', () => {
+    burger.addEventListener("click", () => {
         //toggle nav
-        nav.classList.toggle('nav-active');
+        nav.classList.toggle("nav-active");
 
         //animate links
         navlinks.forEach((link, index) => {
             if(link.style.animation) {
                 link.style.animation = '';
             } else{
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7}s`;
+            link.style.animation = `navLinkFade 0.5s ease forward ${index / 7}s`;
             }    
         });
         //burger animation
-        burger.classList.toggle('toggle');
+        burger.classList.toggle("toggle");
     });
 
 }
+
+//PARALLAX        //element distancr speed
+function parallax(element, distance, speed) {
+    const item = document.querySelector(element);
+    item.style.transform = `translateY(${distance * speed}px)`;
+}
+
+window.addEventListener("scroll", function() {
+    parallax("header", window.scrollY, 0.6);
+    parallax("nav", window.scrollY, 1);
+    parallax(".small-rose", window.scrollY, 0.4);
+    parallax(".big-rose", window.scrollY, 0.2);
+});
+
+
 
 navSlide();
 
